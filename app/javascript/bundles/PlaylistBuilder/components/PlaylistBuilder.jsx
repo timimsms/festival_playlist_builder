0d935f6572959fbb06d12bcd9f051ed23d1e6951
Tim Walsh <tim@useed.org>
2019-01-30 01:04:38 -0700

    Initial API call to Festival#index.json

--- app/javascript/bundles/PlaylistBuilder/components/PlaylistBuilder.jsx
+++ app/javascript/bundles/PlaylistBuilder/components/PlaylistBuilder.jsx
@@ -1,9 +1,19 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 import Button from "@material-ui/core/Button";
+import axios from 'axios';
 
 class PlaylistBuilder extends Component {
+  componentDidMount() {
+    axios.get('http://localhost:3000/sessions/festivals')
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(error => console.log(error))
+  }
+
   // TODO: Add component did mount method.
   render () {
     return (
