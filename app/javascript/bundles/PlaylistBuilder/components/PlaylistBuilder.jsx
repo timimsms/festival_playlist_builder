006a07c9c65ce2b22c3b7765fd2d51a30286d8b8
Tim Walsh <tim@useed.org>
2019-01-30 00:49:37 -0700

    Updates to PlaylistBuilder main JSX

--- app/javascript/bundles/PlaylistBuilder/components/PlaylistBuilder.jsx
+++ app/javascript/bundles/PlaylistBuilder/components/PlaylistBuilder.jsx
@@ -1,14 +1,19 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 
 import Button from "@material-ui/core/Button";
 
-const PlaylistBuilder = () => (
-  <div>
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-  </div>
-);
+class PlaylistBuilder extends Component {
+  // TODO: Add component did mount method.
+  render () {
+    return (
+      <div>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </div>
+    );
+  }
+}
 
 export default PlaylistBuilder;
